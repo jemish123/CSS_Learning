2c33b6b54eddbfa4200aa2bcc20314c163a8a592
jemish123 <jemishgm103@gmail.com>
2025-07-10 00:45:21 +0530

    CSS learning ahead.

--- Form_Designs_CSS/assets/js/script.js
+++ Form_Designs_CSS/assets/js/script.js
@@ -7,32 +7,46 @@ document.addEventListener("DOMContentLoaded", () => {
         borderBottomRightRadius : '20%'
     };
     Object.assign(cover.style, styleToAdd);
+
+
+    document.getElementById("login-content").classList.toggle("active");
 });
 
 function showLoginPanel() {
     const styleToAdd = {
-        left : '0%',
-        borderTopRightRadius : '20%',
-        borderBottomRightRadius : '20%',
-        transform: 'rotateZ(0deg)'
+        left : '50%',
+        borderTopRightRadius : '0%',
+        borderBottomRightRadius : '0%',
+
+        borderTopLeftRadius : '20%',
+        borderBottomLeftRadius : '20%',
     };
     Object.assign(cover.style, styleToAdd);
     
-    document.getElementById('register-content').style.display = 'none';
-    document.getElementById('login-content').style.display = 'block';
+    document.getElementById("login-content").classList.toggle("active");
+    document.getElementById("register-content").classList.toggle("active");
+    
+    // document.getElementById('register-content').style.zIndex = '3';
+    // document.getElementById('login-content').style.zIndex = '-10';
 };
 
 function showRegisterPanel() {
     const styleToAdd = {
-        left : '50%',
+        left : '0%',
+
+        borderTopLeftRadius : '0%',
+        borderBottomLeftRadius : '0%',
+
         borderTopRightRadius : '20%',
         borderBottomRightRadius : '20%',
-        transform: 'rotateZ(180deg)'
     };
     Object.assign(cover.style, styleToAdd);
+
+    document.getElementById("register-content").classList.toggle("active");
+    document.getElementById("login-content").classList.toggle("active");
     
-    document.getElementById('login-content').style.display = 'none';
-    document.getElementById('register-content').style.display = 'inherit';
+    // document.getElementById('login-content').style.zIndex = '3';
+    // document.getElementById('register-content').style.zIndex = '-10';
 }
 
 // document.getElementById('login-btn').addEventListener("click", () => {
